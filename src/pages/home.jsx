e046aa9cfbe7e4dadc7b2ea73b863a5017e568d3
HKAgrwl <harshkumaragarwal2003@gmail.com>
2022-12-09 15:32:50 +0530

    9-Dec

---
+++ src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../css/home.css'
+
+export default function home() {
+  return (
+    <div>
+        <div className="home-main-heading">
+            <h2 className="main-title">Coding Club IIT Jammu</h2>
+        </div>
+    </div>
+  )
+}
