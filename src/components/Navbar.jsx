85568842ff2c7e699d30087a21547a1a434e537a
HKAgrwl <harshkumaragarwal2003@gmail.com>
2022-12-21 23:17:11 +0530

    21Dec-harsh

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,106 @@
-import React from 'react'
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import clubLogo from '../assets/clubLogo.png'
 import { Link } from 'react-router-dom'
 
-export default function Navbar() {
+const navigation = [
+  { name: 'Home', href: '#', current: true,link:"/",color:"lime" },
+  { name: 'Events', href: '#', current: false,link:"/events",color:"purple" },
+  { name: 'Team', href: '#', current: false,link:"/team",color:"yellow" },
+  { name: 'Contact', href: '#', current: false,link:"/contact",color:"rose" },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Example() {
   return (
     <div style={{position:'fixed',width:'100vw',zIndex:'100'}}>
-        <header className="text-gray-100 body-font bg-black">
-   <div className="container mx-auto flex flex-wrap p-2  flex-col md:flex-row items-center">
-   <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0" href='ff'>
-      <img src={clubLogo} alt="clubLogo" style={{height:'50px',width:'60px'}}/>
-    </a>
-    <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-      <Link to="/">
-      <a href="dd" className="mr-8 hover:text-lime-400" style={{fontStyle:'normal',fontWeight:'600'}}>Home</a></Link>
-      <Link to="/events" >
-      <a href="dd" className="mr-8 hover:text-purple-400" style={{fontStyle:'normal',fontWeight:'600'}}>Events</a></Link>
-      <Link to="/team">
-      <a href="dd" className="mr-8 hover:text-yellow-400" style={{fontStyle:'normal',fontWeight:'600'}}>Team</a></Link>
-      <Link to="/contact">
-      <a href="dd" className="mr-8 hover:text-rose-400" style={{fontStyle:'normal',fontWeight:'600'}}>Contact</a></Link>
-    </nav>
-  </div>
-</header>
+    <Disclosure as="nav" className="bg-slate-800" style={{backgroundColor:'black'}}>
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                  <img
+                    className="block h-8 w-auto lg:hidden"
+                    src={clubLogo}
+                    alt="Your Company"
+                  />
+                  <img
+                    className="hidden h-8 w-auto lg:block"
+                    src={clubLogo}
+                    alt="Your Company"
+                  />
+                </div>
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <Link to={item.link}>
+                      <a
+                        key={item.name}
+                        href="/"
+                        className={classNames(
+                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-lime-700 hover:text-white',
+                          'px-3 py-2 rounded-md text-sm font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a></Link>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                {/* <button
+                  type="button"
+                  className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                >
+                  <span className="sr-only">View notifications</span>
+                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                </button> */}
+                
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pt-2 pb-3">
+              {navigation.map((item) => (
+
+                <Link to={item.link}>
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block px-3 py-2 rounded-md text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button></Link>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
     </div>
   )
 }
--- src/components/homeTop.jsx
+++ src/components/homeTop.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import '../css/homeTop.css'
 // import starBG from '../assets/stars.jpg'
+
 import Particle from './Particle'
 
 export default function homeTop() {
   return (
-    <div class="overflow-hidden" style={{height:'750px',boxShadow:'0px 0px 10px 12px'}}>
+    <>
+    <div class="particlejs-top">
         <Particle/>
-        <div className="home-main-heading">
-            <span className="main-title">Coding Club-IIT Jammu</span>
-        </div>
-    </div>
+            <span className="main-title">CODING CLUB-IIT JAMMU</span>
+    </div></>
     
   )
 }
