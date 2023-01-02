8ec6910db72b1ca59fcc2dd75bba81f64964b8c1
Aarav Jain <ajaarav14@gmail.com>
2023-01-02 19:53:19 +0530

    resolved the animation bug

--- src/components/homeTop.jsx
+++ src/components/homeTop.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import '../css/homeTop.css'
+import Typewriter from 'typewriter-effect';
 // import starBG from '../assets/stars.jpg'
 
 import Particle from './Particle'
 
 export default function homeTop() {
+  
   return (
     <>
     <div class="particlejs-top">
         <Particle/>
-            <span className="main-title">CODING CLUB-IIT JAMMU</span>
-    </div></>
+        <div className='heading'>
+        <Typewriter
+  
+       onInit={(typewriter)=> {
+  
+       typewriter
+        
+       .typeString("CODING CLUB-IIT JAMMU")
+       .start();
+       }}
+       />
+        </div>
+    </div>
+    </>
     
   )
 }
